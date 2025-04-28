1c8001c47b3ae528a0ebebe61f01e3741d22352d
MegatronUwU <lorismdr@outlook.fr>
2025-04-28 17:08:10 +0200

    Fix des doors (peut être mauvaise manière de faire)

--- SoulKnightProject/Assets/Scripts/RoomConnector.cs
+++ SoulKnightProject/Assets/Scripts/RoomConnector.cs
@@ -8,6 +8,18 @@ public class RoomConnector : MonoBehaviour
     [SerializeField] private GameObject _doorUp;
     [SerializeField] private GameObject _doorDown;
 
+    [SerializeField] private GameObject _wallLeft;
+    [SerializeField] private GameObject _wallRight;
+    [SerializeField] private GameObject _wallUp;
+    [SerializeField] private GameObject _wallDown;
+
+    private void Awake()
+    {
+        if (_doorLeft != null) _doorLeft.SetActive(false);
+        if (_doorRight != null) _doorRight.SetActive(false);
+        if (_doorUp != null) _doorUp.SetActive(false);
+        if (_doorDown != null) _doorDown.SetActive(false);
+    }
     public Transform GetDoorWaypointTransform(Direction direction)
     {
         foreach (DoorAnchor anchor in _doorAnchors)
@@ -25,15 +37,19 @@ public class RoomConnector : MonoBehaviour
         {
             case Direction.Left:
                 if (_doorLeft != null) _doorLeft.SetActive(true);
+                if (_wallLeft != null) _wallLeft.SetActive(false);
                 break;
             case Direction.Right:
                 if (_doorRight != null) _doorRight.SetActive(true);
+                if (_wallRight != null) _wallRight.SetActive(false);
                 break;
             case Direction.Up:
                 if (_doorUp != null) _doorUp.SetActive(true);
+                if (_wallUp != null) _wallUp.SetActive(false);
                 break;
             case Direction.Down:
                 if (_doorDown != null) _doorDown.SetActive(true);
+                if (_wallDown != null) _wallDown.SetActive(false);
                 break;
         }
     }
