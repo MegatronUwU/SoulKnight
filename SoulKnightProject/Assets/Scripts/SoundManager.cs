a1b5488232ab79d3b5fd221e4f1a0ceae54e30af
MegatronUwU <lorismdr@outlook.fr>
2025-05-20 13:47:01 +0200

    Refacto SoundManager (Object Pooling + Sound Position)

--- SoulKnightProject/Assets/Scripts/SoundManager.cs
+++ SoulKnightProject/Assets/Scripts/SoundManager.cs
@@ -15,9 +15,11 @@ public class SoundManager : MonoBehaviour
     }
 
     [SerializeField] private List<Sound> _sounds;
+    [SerializeField] private int _poolSize = 10;
 
     private Dictionary<string, Sound> _soundMap;
-    private AudioSource _audioSource;
+    private List<AudioSource> _audioSourcePool;
+    private Transform _poolContainer;
 
     private void Awake()
     {
@@ -30,14 +32,53 @@ public class SoundManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
 
-        _audioSource = gameObject.AddComponent<AudioSource>();
         _soundMap = new Dictionary<string, Sound>();
-
         foreach (var sound in _sounds)
             _soundMap[sound.Name] = sound;
+
+        CreateAudioSourcePool();
+    }
+
+    private void CreateAudioSourcePool()
+    {
+        _poolContainer = new GameObject("AudioSourcePool").transform;
+        _poolContainer.parent = transform;
+
+        _audioSourcePool = new List<AudioSource>();
+        for (int i = 0; i < _poolSize; i++)
+        {
+            GameObject go = new GameObject($"PooledAudioSource_{i}");
+            go.transform.parent = _poolContainer;
+            AudioSource src = go.AddComponent<AudioSource>();
+            src.playOnAwake = false;
+            src.spatialBlend = 1f; 
+            _audioSourcePool.Add(src);
+        }
+    }
+
+    private AudioSource GetAvailableAudioSource()
+    {
+        foreach (var src in _audioSourcePool)
+        {
+            if (!src.isPlaying)
+                return src;
+        }
+
+        GameObject go = new GameObject("ExtraAudioSource");
+        go.transform.parent = _poolContainer;
+        AudioSource newSource = go.AddComponent<AudioSource>();
+        newSource.playOnAwake = false;
+        newSource.spatialBlend = 1f;
+        _audioSourcePool.Add(newSource);
+        return newSource;
     }
 
     public void Play(string soundName)
+    {
+        PlayAtPosition(soundName, Camera.main.transform.position);
+    }
+
+    public void PlayAtPosition(string soundName, Vector3 position)
     {
         if (!_soundMap.ContainsKey(soundName))
         {
@@ -46,6 +87,10 @@ public class SoundManager : MonoBehaviour
         }
 
         Sound sound = _soundMap[soundName];
-        _audioSource.PlayOneShot(sound.Clip, sound.Volume);
+        AudioSource src = GetAvailableAudioSource();
+        src.clip = sound.Clip;
+        src.volume = sound.Volume;
+        src.transform.position = position;
+        src.Play();
     }
 }
